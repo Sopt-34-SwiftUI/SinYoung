26c0086049c6393af93e6333f982b3101061a660
ParkSY0919 <qhr498@gmail.com>
2024-05-17 19:36:40 +0900

    [Setting] 프로젝트 생성

---
+++ SOPT_SwiftUIStudy/SOPT_SwiftUIStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOPT_SwiftUIStudy
+//
+//  Created by 박신영 on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
