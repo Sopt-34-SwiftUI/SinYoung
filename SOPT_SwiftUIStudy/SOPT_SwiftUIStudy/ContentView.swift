23c97829f07a10b778ec0544bba7bc63741e5ea6
ParkSY0919 <qhr498@gmail.com>
2024-05-25 02:27:48 +0900

    [Feat] #2 - body 코드 간결화

--- SOPT_SwiftUIStudy/SOPT_SwiftUIStudy/ContentView.swift
+++ SOPT_SwiftUIStudy/SOPT_SwiftUIStudy/ContentView.swift
@@ -8,51 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-       ZStack {
-          Color.black.ignoresSafeArea()
-          VStack {
-             Spacer()
-             HStack{
-                Spacer()
-                Text("990,919")
-                   .padding()
-                   .font(.system(size: 70))
-                   .foregroundColor(.white)
-             }
-             HStack {
-                NumberButton(text: "7")
-                NumberButton(text: "8")
-                NumberButton(text: "9")
-                OperatorButton(text: "X")
-             }
-             
-             HStack {
-                NumberButton(text: "4")
-                NumberButton(text: "5")
-                NumberButton(text: "6")
-                OperatorButton(text: "-")
-             }
-             
-             HStack {
-                NumberButton(text: "1")
-                NumberButton(text: "2")
-                NumberButton(text: "3")
-                OperatorButton(text: "+")
-             }
-             
-             HStack {
-                NumberButton(text: "0")
-                NumberButton(text: "0")
-                NumberButton(text: ".")
-                OperatorButton(text: "=")
-             }
-          }
-          .padding()
-       }
-    }
+   var body: some View {
+      ZStack {
+         Color.black.ignoresSafeArea()
+         VStack {
+            Spacer()
+            ScreenView()
+            NumberPadView()
+         }
+         .padding()
+      }
+   }
 }
-
-//#Preview {
-//    ContentView()
-//}
