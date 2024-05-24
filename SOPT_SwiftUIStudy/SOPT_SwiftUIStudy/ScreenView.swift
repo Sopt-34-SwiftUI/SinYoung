1d9d3df46ffba65c4779176608fa6accbd36d5fa
ParkSY0919 <qhr498@gmail.com>
2024-05-25 02:27:34 +0900

    [Feat] #2 - 기존 body 코드 분리 (Screen)

---
+++ SOPT_SwiftUIStudy/SOPT_SwiftUIStudy/ScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenView.swift
+//  SOPT_SwiftUIStudy
+//
+//  Created by 박신영 on 5/25/24.
+//
+
+import SwiftUI
+
+struct ScreenView: View {
+   var body: some View {
+      HStack {
+         Spacer()
+         Text("990,919")
+            .padding()
+            .font(.system(size: 70))
+            .foregroundColor(.white)
+      }
+   }
+}
